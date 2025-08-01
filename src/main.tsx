c9993a575717ea7a52f258179707b1dd51dda3d1
Jmeow <j@jmeow.net>
2025-08-01 17:11:30 -0500

    Add basic landing page and a tiny bit of routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import "dreamland"
+import { Landing } from "./pages/landing.tsx"
+const path = location.pathname
 const App: Component = function () {
-    return <div>Hello world</div>
+    return <div>{$if(path == "/", <Landing />)}</div>
 }
 
 window.addEventListener("load", () => {
---
+++ src/pages/landing.tsx
@@ -0,0 +1,11 @@
+export const Landing: Component = function () {
+    return (
+        <div>
+            <h1>Lack</h1>
+            <h2>An alternate Slack client</h2>
+            <a href="/login">Log in</a>
+            <br />
+            <em>PS: CSS is coming soon.</em>
+        </div>
+    )
+}
